a4b45394a0a0e55bfc3b52df3eafdde8c3bcb967
Artemis <minhquan.nguyen.fr@gmail.com>
2020-03-12 11:05:32 +0100

    TP

---
+++ js/home.js
@@ -0,0 +1,14 @@
+/* 
+$(window).resize(function () {
+    console.log("Resize")
+    $('#ava2').css({
+        'position': 'absolute',
+        'right': parseInt(window.innerWidth / 2),
+        'bottom': parseInt(window.innerHeight / 2)
+    });
+    $('.intro').css({'position' : 'absolute',
+    'right': parseInt(window.innerWidth/2),
+                  'bottom': parseInt(window.innerHeight/2 + 20 + 'px')
+                 });
+});
+*/
